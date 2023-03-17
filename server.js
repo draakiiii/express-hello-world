96d44df3b2f869a0fe03e14fcefd9e8809cd909d
draakiiii <ordenadorsolo@gmail.com>
2023-03-17 01:36:22 +0100

    server fixed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./app')
 
 const port = process.env.PORT || 3000
+const app = express(); // Asegúrate de que esta línea esté presente y correctamente definida
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
